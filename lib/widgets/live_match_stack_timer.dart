fb9194aaa51dd720bc5e7432dafc0da12ccdcfde
SolihCoder <BilolMuhammadSolih@gmail.com>
2025-07-16 19:57:58 +0500

    some changes

--- lib/widgets/live_match_stack_timer.dart
+++ lib/widgets/live_match_stack_timer.dart
@@ -13,39 +13,45 @@ class LiveMatchStackTimer extends StatelessWidget {
     return Center(
       child: ClipRRect(
         borderRadius: BorderRadiusGeometry.circular(12),
-        child: SizedBox(
-          width: 176.w,
-          height: 78.h,
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 44, sigmaY: 44),
-            child: Container(
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: (){},
+            child: SizedBox(
               width: 176.w,
               height: 78.h,
-              decoration: BoxDecoration(
-                color: Colors.white.withValues(alpha: 0.2),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                spacing: 6,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "This match will start in:",
-                    style: TextStyle(
-                      color: Color(0xFFEAEAEA),
-                      fontSize: 14.r,
-                      fontWeight: FontWeight.w600,
-                    ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 44, sigmaY: 44),
+                child: Container(
+                  width: 176.w,
+                  height: 78.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withValues(alpha: 0.2),
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  Text(
-                    "22:19:32",
-                    style: TextStyle(
-                      color: Color(0xFFEAEAEA),
-                      fontSize: 24.r,
-                      fontWeight: FontWeight.w600,
-                    ),
+                  child: Column(
+                    spacing: 6,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "This match will start in:",
+                        style: TextStyle(
+                          color: Color(0xFFEAEAEA),
+                          fontSize: 14.r,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "22:19:32",
+                        style: TextStyle(
+                          color: Color(0xFFEAEAEA),
+                          fontSize: 24.r,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
